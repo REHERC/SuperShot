d7eec6c9aae7b16e3ddd019087c8e06215a4f714
REHERC <amaury.vanoorenberghe@yahoo.com>
2018-08-12 23:34:50 +0200

    Id on't think there will ever be another commit (except for release 1.0 setpember 18th if it breaks the plugin)

---
+++ SuperShot/Photon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Spectrum.API.Interfaces.Plugins;
+using Spectrum.API.Interfaces.Systems;
+using UnityEngine;
+
+namespace SuperShot
+{
+    public class Photon : IPlugin
+    {
+        public void Initialize(IManager manager, string ipcIdentifier)
+        {
+            Console.WriteLine($"Initializing ... ({ipcIdentifier})");
+            CurrentPlugin.Initialize();
+            CurrentPlugin.Log.Info("Initialization done!");
+            CurrentPlugin.Log.Info("Adding key bindings ...");
+
+            Parameters();
+
+            manager.Hotkeys.Bind(CurrentPlugin.Config.GetItem<string>("KeyBinding"), () =>
+            {
+                TakeScreenshot();
+            });
+            CurrentPlugin.Log.Info("Added key bindings!");
+        }
+
+        private void Parameters()
+        {
+            SuperSize = Math.Max(CurrentPlugin.Config.GetItem<int>("ScaleFactor"), 1);
+        }
+
+
+
+        private int SuperSize = 1;
+        
+        private void TakeScreenshot()
+        {
+            string path = CurrentPlugin.Files.RootDirectory + @"\Data";
+            string name = $"Screenshot {DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss")}";
+
+            if (!Directory.Exists(path)) {
+                Directory.CreateDirectory(path);
+            }
+            
+            UnityEngine.Application.CaptureScreenshot($@"{path}\{name}.png", SuperSize);
+        }
+    }
+}
---
+++ SuperShot/Storage/CurrentPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Spectrum.API.Configuration;
+using Spectrum.API.Logging;
+using Spectrum.API.Storage;
+
+namespace SuperShot
+{
+    class CurrentPlugin
+    {
+        public static void Initialize()
+        {
+            Log = new Logger("SuperShot.log");
+            Log.WriteToConsole = true;
+            Config = new Settings("Config");
+            InitConfig();
+            Files = new FileSystem();
+        }
+
+        public static void InitConfig()
+        {
+            var DefaultConfig = new Dictionary<string, object>
+            {
+                {"KeyBinding", "LeftControl+Alpha1"},
+                {"ScaleFactor", 2}
+            };
+
+            foreach (var entry in DefaultConfig)
+            {
+                if (!Config.ContainsKey(entry.Key))
+                {
+                    Config.Add(entry.Key, entry.Value);
+                }
+            }
+
+            Config.Save();
+        }
+
+        public static Logger Log;
+        public static Settings Config;
+        public static FileSystem Files;
+    }
+}
